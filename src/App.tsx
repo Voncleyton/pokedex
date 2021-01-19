3edf83df5d76a8fa130b2301b509d01428e5136e
Voncleyton <martins.voncleyton@gmail.com>
2021-01-19 18:55:27 -0400

    initial commit - basic structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <View />
+    </>
+  );
+};
+
+export default App;
